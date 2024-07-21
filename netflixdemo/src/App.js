5158f5f75baa2151470b00db2b3c1b52ac6af8e7
sakshi <sshah380008@gmail.com>
2024-07-21 13:27:35 +0530

    first commit

---
+++ netflixdemo/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-400 bg-red-700 m-5">
+   hello
+    </div>
+  );
+}
+
+export default App;
